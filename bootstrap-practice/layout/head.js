3b45ae3f95302dcde907a0583a2e9eaa74515d4c
Thomas Dolan <dolatr@mail.broward.edu>
2026-02-15 14:59:12 -0500

    Inluded a favicon and adjusted media query for showing content on md screens

--- bootstrap-practice/layout/head.js
+++ bootstrap-practice/layout/head.js
@@ -10,6 +10,12 @@ export function renderHead() {
     <link rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.13.1/font/bootstrap-icons.min.css"
       crossorigin="anonymous">
+    
+    <link rel="icon" type="image/svg+xml" href="data:image/svg+xml,
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'>
+    <rect width='16' height='16' fill='%236c757d'/>
+    <text x='8' y='12' font-size='10' text-anchor='middle' fill='white'>B</text>
+    </svg>">
 
     <!-- Custom CSS -->
     <link rel="stylesheet" href="main.css">
